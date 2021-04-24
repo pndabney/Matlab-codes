4ab84c7fbe75a16631341233a319f13b73b3d1bf
Page Dabney <pdabney@cressida.princeton.edu>
2021-04-24 17:27:52 -0400

    Optional plot added

--- mgaps_m2.m
+++ mgaps_m2.m
@@ -1,27 +1,36 @@
-function varargout=m2_mgaps(sd,num,d)
-% [C,pmis,SData]=M2_MGAPS(sd,num,d)
+function varargout=mgaps_m2(sd,num,d,p)
+% [C,pmis,SData]=MGAPS_M2(sd,num,d,p)
 %
 % Takes a time series and creates synthetic gaps
 %
 % INPUT:
 %
 % sd               Seismic data array (1-D)
-% num              Number of gaps to create
+% num              Number of gaps to create (default: 10)
 % d                Distribution of gaps
-%                  'rgaps' uniformly random distributed gaps
+%                  'rgaps' uniformly random distributed gaps (default)
 %                  'egaps' evenly distributed gaps
+% p                1 makes a plot
+%                  0 does not make a plot (default)
 %
 % OUTPUT:
 %
 % C                Cell array of the remaining segments
 % pmis             Percentage of missing data
+% SData            New seismic array with gaps (use for plots)
 %
 % EXAMPLE:
 %
 % sd = rand(1028,1); num = randi(10); d = 'rgaps';
-% [C,pmis,~]=m2_mgaps(sd,num,d);
+% [C,pmis,~]=mgaps_m2(sd,num,d);
 %
 % Last modified by pdabney@princeton.edu 04/10/21
+
+% Default values
+defval('p',0);
+defval('num',10);
+defval('d','rgaps');
+
 
 if isstr(d) & d=='rgaps'
     ng = sort(randi([1 length(sd)],1,2*num));
@@ -54,11 +63,14 @@ C{num+1} = SData(ng(end)+1:length(sd));
 pmis = ((length(sd)-sum(abs(~isnan(SData))))/length(sd)) * 100;
  
 
-% Create plot
-plot(SData);
-axis tight;
-xlabel('Time (s)');
-title('Segmented Data');
+% Optional plot
+if p == 1
+    plot(SData);
+    axis tight;
+    xlabel('Time (s)');
+    title('Segmented Data');
+end
+
 
 % Optional output
 varns={C,pmis,SData};
