5627babfd9cf547a06d55a0a107b4c275b4ad38c
Page Dabney <pdabney@cressida.princeton.edu>
2021-07-29 09:46:37 -0400

    Added ability to specify output units

--- getfreqs.m
+++ getfreqs.m
@@ -1,8 +1,8 @@
-function varargout=getfreqs(frange,mtype,dset)
-% [freqs,mlabel,tdata]=getfreqs(frange,mtype,dset)
+function varargout=getfreqs(frange,mtype,dset,units)
+% [freqs,mlabel,tdata]=getfreqs(frange,mtype,dset,units)
 %
 % Reads in file 'masterswidmer' and extracts mode frequencies for specified range and types.
-% NOTE: Assumes you have the data file, 'masterwidmer'. Data units are in microhertz.
+% NOTE: Assumes you have the data file, 'masterwidmer'. 
 %
 % Input:
 %
@@ -10,10 +10,11 @@ function varargout=getfreqs(frange,mtype,dset)
 % mtype         Type of modes: 'spheriodal','toroidal','both'
 % dset          0 prem modes
 % 		1 observed modes
+% units         Output units: 'hertz','millihertz','microhertz'
 %
 % Output;
 %
-% freqs         Vector containing mode frequencies of interest (in microhertz)
+% freqs         Vector containing mode frequencies of interest 
 % mlabel        String array with the labels to the corresponding mode frequency       
 % tdata         Data table containing the observed modes, their uncertainity, and the prem computed
 %               modes (in microhertz)
@@ -23,10 +24,16 @@ function varargout=getfreqs(frange,mtype,dset)
 % Data table from Free Oscillations: Frequencies and Attenuations by Masters and Widmer.
 % Requires repository slepian_alpha. See defval.
 %
-% Last modified by pdabney@princeton.edu, 7/27/21
+% Last modified by pdabney@princeton.edu, 7/29/21
 
 % Default values
 defval('dset',1)
+
+% Unit conversions
+% conversion to hertz
+chz = 1e-6;
+% conversion to millihertz
+cmhz = 1e-3;
 
 % Ensure there are only 2 frequencies
 if length(frange) == 2
@@ -73,6 +80,15 @@ if dset == 1
     freqs = tdata(:,1);
 elseif dset == 0
     freqs = tdata(:,3);
+end
+
+% Convert to specified output units
+if strcmp(units,'hertz') == 1
+    freqs = freqs*chz;
+elseif strcmp(units,'millihertz') == 1
+    freqs = freqs*cmhz;
+elseif strcmp(units,'microhertz') == 1
+    % Do nothing
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
