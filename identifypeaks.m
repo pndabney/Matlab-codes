96e1847a0c74d47e1b22550e9a06c40abc0b8d93
Page Dabney <pdabney@cressida.princeton.edu>
2021-08-19 22:05:54 -0400

    Modified method of identifying peaks and increase speed

--- identifypeaks.m
+++ identifypeaks.m
@@ -1,5 +1,5 @@
-function varargout=identifypeaks(x,y,freqs,thresh,ptype,units)
-% [pks,locs,width,prom,nfreqs,ipks,P1]=IDENTIFYPEAKS(x,y,freqs,thresh,ptype)
+function varargout=identifypeaks(x,y,freqs,frange,thresh,ptype,units)
+% [pks,locs,width,prom,nfreqs,ipks,P1]=IDENTIFYPEAKS(x,y,freqs,frange,thresh,ptype,units)
 %
 % Identify and plot peaks given a specified threshold.
 %
@@ -8,6 +8,7 @@ function varargout=identifypeaks(x,y,freqs,thresh,ptype,units)
 % y              Data (1-D)
 % x              Corresponding x-axis data (1-D)
 % freqs          Vector containing mode frequencies of interest
+% frange         Frequency range of interest, in hertz (format: [f1 f2])
 % thresh         Minimum height difference between a peak and its neighbors
 % ptype          0 regular plot
 %                1 log scale on x-axis
@@ -28,10 +29,9 @@ function varargout=identifypeaks(x,y,freqs,thresh,ptype,units)
 %
 % NOTE:
 %
-% In the frequency range of 0.2-1.0 mHz (0S2 0T2 2S1 0S3 0T3 0S4 1S2 0T4 0S0 0S5 1S3 3S1 2S2)
-% Data from  Free Oscillations: Frequencies and Attenuations by Masters and Widmer 1995
+% Data from  Free Oscillations: Frequencies and Attenuations by Masters and Widmer 1995.
 %
-% Last modified by pdabney@princeton.edu, 6/29/21
+% Last modified by pdabney@princeton.edu, 8/19/21
 
 % Default values
 defval('ptype',2)
@@ -55,66 +55,33 @@ freqs = freqs*ucon;
 freqd = 9.5*1e-6*ucon;
 % Lower bound frequency adjustment
 freqb = 1e-5*ucon;
-% Threshold for prominence
-threshp = 5e8;
+
 % Adjust x and y limits to correspond with conversion
-exlim = [0.0002 0.001]*ucon;
-
+exlim = frange*ucon; % Will need to make this an input range in the future
+in = find(x >= exlim(1) & x <= exlim(2));
 % y limits for ptype=0,1
-p01ylim = [-5e8 12e9];
+p01ylim = [-5e8 max(y(in))+1e8];
 % y limits for ptype=2,3
-p23ylim = [5e4 max(y)];
+p23ylim = [min(y(in))+5e4 max(y(in))+5e9];
 
-
+%-------------------------------------------------------------------------------------
 % Find peaks in the data
-[pks, locs, width, prom] = findpeaks(y,x,'Threshold',thresh); 
+[pks, locs, width, prom] = findpeaks(y,x,'MinPeakHeight',thresh); 
 freqrange = zeros(length(freqs),2);
-i = 1:length(freqs);
-freqrange(i,1) = freqs(i) + freqd;
-freqrange(i,2) = freqs(i) - freqd;
-
+freqrange(:,1) = freqs + freqd;
+freqrange(:,2) = freqs - freqd;
 % Eliminate peaks that are not within a specified frequency range
 % Deal with the upperbound
-for k = length(locs):-1:1
-    if (locs(k) > freqrange(length(freqs),1))
-        locs(k)=[];
-        width(k)=[];
-        prom(k)=[];
-        pks(k)=[];
-    end
-end
-
+Ku=find(locs > freqrange(length(freqs),1));
+locs(Ku)=[]; pks(Ku)=[]; prom(Ku)=[]; width(Ku)=[];
 % Deal with the middle section
 for j = 1:length(freqs)-1
-    for k = length(locs):-1:1
-        if locs(k) > freqrange(j,1) && locs(k) < freqrange(j+1,2)
-            locs(k)=[];
-            width(k)=[];
-            prom(k)=[];
-            pks(k)=[];
-        end
-    end
+    Km = find(locs > freqrange(j,1) & locs < freqrange(j+1,2));
+    locs(Km)=[]; pks(Km)=[]; prom(Km)=[]; width(Km)=[];
 end
-
 % Need to adjust the lower bound for this specific frequency range
-for k = length(locs):-1:1
-    if (locs(k) < freqrange(1,2) - freqb) 
-        locs(k)=[];
-        width(k)=[];
-        prom(k)=[];
-        pks(k)=[];
-    end
-end
-
-% Eliminate peaks based on prominence
-for k = length(prom):-1:1
-    if prom(k) < threshp
-       prom(k)=[];
-       locs(k)=[];
-       width(k)=[];
-       pks(k)=[];
-   end
-end
+Kl=find(locs < freqrange(1,2) - freqb);
+locs(Kl)=[]; pks(Kl)=[]; prom(Kl)=[]; width(Kl)=[];
 
 % Additional info
 % Peaks identified
@@ -122,7 +89,7 @@ ipks = length(locs);
 % Number of frequencies 
 nfreqs = length(freqs);
 
-%--------------------------------------------------------------------------------
+%------------------------------------------------------------------------------------
 % Plot of mode frequencies in power spectrum
 f = figure('Units', 'centimeters', 'Position', [0.05, 2.8, 18, 16], 'PaperPositionMode','Auto');
 %figure(gcf); clf;
@@ -130,37 +97,30 @@ set(gca, 'LineWidth', 1.25, 'FontSize', 8);
 hx1 = subplot(211);
 set(hx1,'Position',[0.15, .57, .8, .32]);
 ax1 = gca; 
-%ax1.XAxis.Exponent=0; %ax1.YAxis.Exponent=0;
 if ptype == 0
     P1=plot(x,y,'LineWidth',2);    
     hold on;
-    for i = 1:length(freqs)
-        p1=plot([freqs(i) freqs(i)], p01ylim, 'r', 'LineWidth',0.1);
-    end
+    p1=plot([freqs freqs], p01ylim, 'r', 'LineWidth',0.1);
     hold off;
     ylim(p01ylim)
 elseif ptype == 1
     P1=semilogx(x,y,'LineWidth',2);
     hold on;
-    for i = 1:length(freqs)
-        p1=plot([freqs(i) freqs(i)], p01ylim, 'r', 'LineWidth',0.1);
-    end
+    p1=plot([freqs freqs], p01ylim, 'r', 'LineWidth',0.1);
     hold off;
     ylim(p01ylim)
 elseif ptype == 2
     P1=semilogy(x,y,'LineWidth',2);
     hold on;
-    for i = 1:length(freqs)
-        p1=plot([freqs(i) freqs(i)], p23ylim, 'r', 'LineWidth',0.1);
-    end
+    p1=plot([freqs freqs], p23ylim, 'r', 'LineWidth',0.1);
     hold off;
+    ylim(p23ylim)
 elseif ptype == 3
     P1=loglog(x,y,'LineWidth',2);    
     hold on;
-    for i = 1:length(freqs)
-        p1=loglog([freqs(i) freqs(i)], p23ylim, 'r', 'LineWidth',0.1);
-    end
+    p1=loglog([freqs freqs], p23ylim, 'r', 'LineWidth',0.1);
     hold off;
+    ylim(p23ylim)
 end
 uistack(P1,'top');
 xlim(exlim)
@@ -168,41 +128,34 @@ ax1.XGrid = 'off'; ax1.YGrid = 'on';
 set(ax1,'xticklabel',[]);
 ylabel({'Spectral Density', '(Energy/Hz)'});
 if exist('p1')
-    legend([p1],{'Widmer & Masters'});
+    legend({'Widmer & Masters'});
 end
 hx2 = subplot(212);
 set(hx2,'Position',[0.15, .22, .8, .32]);
-%hx2.XAxis.Exponent=0; %hx2.YAxis.Exponent=0;
 if ptype == 0
     P2=plot(x,y,'LineWidth',2);
     hold on;
-    for i = 1:length(locs)
-        p2=plot([locs(i) locs(i)], p01ylim, 'k', 'LineWidth', 0.1);
-    end
+    p2=plot([locs locs], p01ylim, 'k', 'LineWidth', 0.1);
     hold off;
     ylim(p01ylim)
 elseif ptype == 1
     P2=semilogx(x,y,'LineWidth',2);
     hold on;
-    for i = 1:length(locs)
-        p2=plot([locs(i) locs(i)], p01ylim, 'k', 'LineWidth', 0.1);
-    end
+    p2=plot([locs locs], p01ylim, 'k', 'LineWidth', 0.1);
     hold off;
     ylim(p01ylim)
 elseif  ptype == 2
     P2=semilogy(x,y,'LineWidth',2);
     hold on;
-    for i = 1:length(locs)
-        p2=plot([locs(i) locs(i)], p23ylim, 'k', 'LineWidth', 0.1);
-    end
+    p2=plot([locs locs], p23ylim, 'k', 'LineWidth', 0.1);
     hold off;
+    ylim(p23ylim)
 elseif ptype == 3
     P2=loglog(x,y,'LineWidth',2);
     hold on;
-    for i = 1:length(locs)
-        p2=plot([locs(i) locs(i)], p23ylim, 'k', 'LineWidth', 0.1);
-    end
+    p2=plot([locs locs], p23ylim, 'k', 'LineWidth', 0.1);
     hold off
+    ylim(p23ylim)
 end
 uistack(P2,'top');
 hx2.XGrid = 'off'; hx2.YGrid = 'on';
@@ -220,14 +173,13 @@ else
     warning('Frequency units unspecified')
     xlabel('Frequency')
 end
+
 if exist('p2')
-    legend([p2],{'Estimated'});
+    legend({'Estimated'});
 end
 
-
+%-----------------------------------------------------------------------------------------
 % Optional output
-varns={pks,locs,width,prom,nfreqs,ipks,P1};
+varns={pks,locs,width,prom,freqs,nfreqs,ipks,P1};
 varargout=varns(1:nargout);
 end
-
-
