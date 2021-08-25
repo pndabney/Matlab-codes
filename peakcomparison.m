afb8aa6fbdc0a3a0bcec8ad497cd506b5d9682c9
Page Dabney <pdabney@cressida.princeton.edu>
2021-08-25 16:53:25 -0400

    Updated output

--- peakcomparison.m
+++ peakcomparison.m
@@ -1,37 +1,46 @@
-function varargout=peakcomparison(freqs,locs)
-% [f,err]=peakcomparison(freqs,locs)
+function varargout=peakcomparison(freqs,pkloc)
+% [f,locs,err]=peakcomparison(freqs,pkloc)
 %
 % Determines which peak identified best corresponds with a mode frequency of interest
 %
 % Input:
 %
 % freqs            Array containing mode frequencies of interest
-% locs             Array of peak locations 
+% pkloc            Array of peak locations 
 %
 % Output:
 %
 % f                Mode frequencies likely identified
-% err              Difference in observed versus mode frequencies
+% locs             Peak locations that likely correspond to a known mode frequency
+% err              Difference in observed versus known mode frequencies
 %
-% Last modified by pdabney@princeton.edu, 8/24/21
+% Last modified by pdabney@princeton.edu, 8/25/21
 
 % Ensure locs is not NaN
-if isnan(locs) == 1
-    error('Peak location input cannot be NaN')
-end
+if isnan(pkloc) == 1
+    f = [];
+    err = [];
+else
+    % Determine which peak location is likely a mode frequency
+    for i = 1:length(freqs)
+        d = abs(freqs(i)-pkloc);
+        [~,Ind(i)]=min(d);
+    end
+    locs = pkloc(unique(Ind));
+    
+    % Determine the mode frequencies identified
+    for i = 1:length(locs)
+        d = abs(freqs-locs(i));
+        [~,id(i)]=min(d);
+    end 
+    f = freqs(unique(id));
 
-% Determine which peak location is likely a mode frequency
-for i = 1:length(locs)
-    d = abs(freqs-locs(i));
-    [~,Ind(i)]=min(d);
+    % Difference in observed versus mode frequencies
+    err = abs(f-locs);
 end
-f = freqs(Ind);
-
-% Difference in observed versus mode frequencies
-err = abs(f-locs);
 
 % Optional Output
-vars={f,err};
+vars={f,locs,err};
 varargout=vars(1:nargout);
 
 end
