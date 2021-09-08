4964b2f53426c8c92101524562d3fa7a95f11e29
Page Dabney <pdabney@cressida.princeton.edu>
2021-09-08 10:19:28 -0400

    Updated to comments and removed defaults

--- choosecomp.m
+++ choosecomp.m
@@ -1,18 +1,18 @@
 function varargout=choosecomp(Xi1,Xi2,comp)
 % [tseries1,tseries2]=CHOOSECOMP(Xi1,Xi2,comp)
 %
-% Reads two three-component time series and picks two components
+% Takes two three-component time series folders and picks files for two components 
 %
 % INPUT:
 %
 % Xi1,Xi2             Data directories containing the three components of a seismogram,
 %                     full path included
 % comp                Choose two components (UU=1, UV=2, UW=3, VU=4, VV=5, VW=6, WU=7,
-%                     WV=8, WW=9) [default = 1]
+%                     WV=8, WW=9) 
 %
 % OUTPUT:
 %
-% tseries1,tseries2   Filenames of SAC data for the choosen time series of interest   
+% tseries1,tseries2   Filenames of SAC data for the choosen time series, full path included   
 %
 % NOTE:
 %
@@ -22,10 +22,7 @@ function varargout=choosecomp(Xi1,Xi2,comp)
 % Xi2 = '/data2/InSight/ce_IPGP_deglitched/S0132_IPGP_corr';
 % [tseries1,tseries2]=choosecomp(Xi1,Xi2,1)
 %
-% Last modified by pdabney@princeton.edu, 03/27/21
-
-% Default
-defval('comp',1);
+% Last modified by pdabney@princeton.edu, 09/08/21
 
 % Read in data
 dXi1 = dir(Xi1);
@@ -35,13 +32,13 @@ dXi2 = dir(Xi2);
 dXi1(1:2,:)=[];
 dXi2(1:2,:)=[];
 
-
 % Creates two matrices
 for i =1:3
     ts1(:,i) = dXi1(i);
     ts2(:,i) = dXi2(i);
 end
 
+% Determines file names for two components of interest
 if comp == 1
    tseries1 = fullfile(ts1(1).folder,ts1(1).name);
    tseries2 = fullfile(ts2(1).folder,ts2(1).name);
@@ -72,7 +69,6 @@ elseif comp == 9
  else
    warning('Does not exist')
 end
-
 
 % Optional output
 varns={tseries1,tseries2};
