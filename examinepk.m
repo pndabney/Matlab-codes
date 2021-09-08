e57cade5797d32f848cc89c3eba01665854a8085
Page Dabney <pdabney@cressida.princeton.edu>
2021-09-08 11:08:07 -0400

    Updated comments and added struct for output

--- examinepk.m
+++ examinepk.m
@@ -1,84 +1,76 @@
 function varargout=examinepk(x,y,freq,frange,ptype,thresh)
-% [X,Y,sigma,mu,g,pks,locs,wdt,prm,Ra,skw,kurt]=examinepk(x,y,freq,frange,ptype,thresh)
+% [Y,X,g,peak,gauss]=examinepk(x,y,freq,frange,ptype,thresh)
 %
 % Examines the size and shape, mean and standard deviation, and fits a gaussian distribution to
 % a single peak of interest. 
 %
 % Input:
 %
-% y              Data (1-D)
-% x              Corresponding x-axis data (1-D)
+% y              Data array (1-D)
+% x              Corresponding x-axis array (1-D)
 % freq           Mode frequency of interest
-% frange         Frequency range for window of interest 
-% ptype          0 regular plot
-%                1 log scale on x-axis [default]
-%                2 log scale on y-axis
-%                3 log-log scale plot
+% frange         Index for frequency range 
 % thresh         Threshold, minimum peak height
 %
 % Output:
 %
-% sigma          Standard Deviation (1 spectral density, 2 gaussian function)
-% mu             Mean (1 spectral density, 2 gaussian function)
+% Y              Data array of only specifed frequency range of interest (1-D)
+% X              Corresponding x-axis array (1-D)
 % g              Gaussian Curve
-% pks            Peak height (1 spectral density, 2 gaussian function)
-% locs           Location of peak (1 spectral density, 2 gaussian function) 
-% wdt            Full width of peak at the half maximum 
-%                (1 spectral density, 2 gaussian function)
-% Area           Area beneath the curve
-%                (1 spectral density, 2 gaussian function)
-% Ra             Ratio of the area of the LHS to the RHS
-% skw            Skewness, measure of asymmetry of the data 
-%                (1 spectral density, 2 gaussian function)
-% kurt           Kurtosis, measure of how outlier-prone a distribution 
-%                (1 spectral density, 2 gaussian function)
-% 
-% Last modified by pdabney@princeton.edu, 7/16/21
-
+% peak           Struct of information about peak of interest
+% gauss          Struct of information about gaussian distribution 
+%
+% Last modified by pdabney@princeton.edu, 9/8/21
 
 % Compute Gaussian Distribution
 % Does not create a plot through fitgauss
 plotornot = 0;
-[X,Y,cfit,e,res]=fitgauss(y,x,freq,frange,thresh,plotornot);  
-
+[X,Y,cfit,~,~]=fitgauss(y,x,freq,frange,thresh,plotornot);  
 % Variables for Gaussian function
 a1 = cfit.a1; b1 = cfit.b1; c1 = cfit.c1;
 g = a1*exp(-((X-b1)/c1).^2);
 
-
 % Find peak,location, width, prominence
-[pks1,locs1,wdt1,prm1]=findpeaks(Y,X,'MinPeakHeight',thresh,'WidthReference','halfheight');
-[pks2,locs2,wdt2,prm2]=findpeaks(g,X,'MinPeakHeight',thresh,'WidthReference','halfheight');
-pks=[pks1 pks2]; locs=[locs1 locs2]; wdt=[wdt1 wdt2]; prm=[prm1 prm2];
+[pks1,locs1,wdt1,~]=findpeaks(Y,X,'MinPeakHeight',thresh,'WidthReference','halfheight');
+[pks2,locs2,wdt2,~]=findpeaks(g,X,'MinPeakHeight',thresh,'WidthReference','halfheight');
 
 
 % Find the mean and standard deviation for the Data and Gaussian distribution
-mu = [mean(Y) mean(g)];
-sigma = [std(Y) std(g)];
-
-% Find Area
+mu = [mean(Y) mean(g)]; sigma = [std(Y) std(g)];
+% Find Area of the data and the gaussian distribution
 Area = [trapz(X,Y) trapz(X,g)];
-
 % Determine how outlier prone the distribution is
 kurt = [kurtosis(Y) kurtosis(g)];
 % Measure of the asymmetry of the data around the sample mean
 skw = [skewness(X) skewness(g)];
 
-
 % Determine how symmetric the peak is
 % Center peak location index
-cp = find(X == locs(1),1);
+cp = find(X == locs1,1); cpg = find(X == locs2,1);
 % Area of the RHS
-A1 = trapz(X(cp:end),Y(cp:end));
+A1 = [trapz(X(cp:end),Y(cp:end)) X(cpg:end),g(cpg:end))];
 % Area of the LHS
-A2 = trapz(X(1:cp),Y(1:cp));
+A2 = [trapz(X(1:cp),Y(1:cp)) X(1:cpg),g(1:cpg))];
 % Ratio of the area of the  RHS and LHS 
-% if R > 1, A1 > A2; if R < 1, A1 < A2
-Ra = A1/A2;
+% (if R > 1, A1 > A2; if R < 1, A1 < A2)
+Ra = [A1(1)/A2(1) A1(2)/A2(2)];
+
+
+% Create struct for output
+% Gaussian curve
+gauss.height = pks2; gauss.location = locs2; gauss.width = wdt2;
+gauss.mu = mu(2); gauss.sigma = sigma(2); 
+gauss.kurtosis = kurt(2); gauss.skewness = skew(2);
+gauss.area = Area(2); gausa.RA = Ra(2);
+% Identified peak
+peak.height = pks1; peak.location = locs1; peak.width = wdt1;
+peak.mu = mu(1); peak.sigma = sigma(1);
+peak.kurtosis = kurt(1); peak.skewness = skew(1);
+peak.area = Area(1); peak.Ra = Ra(1);
 
 
 % Optional Output
-vars={X,Y,sigma,mu,g,pks,locs,wdt,prm,Area,Ra,skw,kurt};
+vars={X,Y,g,peak,gauss};
 varargout=vars(1:nargout);
 
 end
