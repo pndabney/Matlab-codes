4c48b58a15bcedec63a4605cb6e9a7b39f0ac939
Page Dabney <pdabney@cressida.princeton.edu>
2021-09-10 10:21:54 -0400

    Plots and compares identified peaks with known mode frequencies

--- plotidpks.m
+++ plotidpks.m
@@ -19,7 +19,10 @@ function varargout=plotidpks(x,y,locs,freqs,frange,ptype)
 %
 % P1        Used for plotting
 %
-% Last modified by pdabney@princeton.edu, 9/9/21
+% Last modified by pdabney@princeton.edu, 9/10/21
+
+% Y limit constant as a buffer for plotting (change as needed)
+ybuff = 1e8;
 
 % Ensure f1 < f2 in frange
 frange = sort(frange);
@@ -30,75 +33,105 @@ X = x(in); Y = y(in);
 
 % Get y-axis limits for different plot types
 % y limits for NO y-axis log scale
-nlylim = [min(Y)-1e8 max(Y)+1e8];
+nlylim = [min(Y)-ybuff max(Y)+ybuff];
 % y limits for y-axis log scale
-lsylim = [min(Y)+5e4 max(Y)+5e9];
+lsylim = [min(Y) max(Y)+ybuff];
+
+% Determine y limits 
+if (ptype == 0 || ptype == 1)
+    ylimits = nlylim;
+elseif (ptype == 2 || ptype == 3)
+    ylimits = lsylim;
+end
+
+% Determine upper x-axis scale
+if (ptype == 0 || ptype== 2)
+    xscale = 'linear';
+elseif (ptype == 1 || ptype == 3)
+    xscale = 'log';
+end
 
 % Number of peaks identified 
 ipks = length(locs);
 
 %-----------------------------------------------------------------------------------------------------------
 % Set up figure
-f = figure('Units', 'centimeters', 'Position', [0.05, 2.8, 18, 16], 'PaperPositionMode','Auto');
-%figure(gcf); clf;
-set(gca, 'LineWidth', 1.25, 'FontSize', 8);
+figure('Units', 'centimeters', 'Position', [0.05, 2.8, 20, 16], 'PaperPositionMode','Auto')
 
+% Insert invisible axes to connect lines from the bottom plots to the top
+axes('Position',[0.15, .521, .8, .049]); 
+if (ptype == 0 || ptype== 2)
+   plot([locs'; locs'], [0 1], ':k', 'LineWidth', 0.5); xlim([X(1) X(end)]);
+elseif (ptype == 1 || ptype == 3)
+   semilogx([locs'; locs'], [0 1], ':k', 'LineWidth', 0.5); xlim([X(1) X(end)]);
+end
+set(gca,'visible','off');
+
+%------------------------------------------------------------------------------------------------------
 % Plot of known mode frequencies
+% Set up first subplot
 hx1 = subplot(211);
 set(hx1,'Position',[0.15, .57, .8, .32]);
-ax1 = gca;
+
+% Plot the data and known modes
 if ptype == 0
-    P=plot(X,Y,'LineWidth',2); hold on;
-    ylimits = nlylim;
+    P=plot(X,Y,'LineWidth',1.5); hold on;
 elseif ptype == 1
-    P=semilogx(X,Y,'LineWidth',2); hold on;
-    ylimits = nlylim;
+    P=semilogx(X,Y,'LineWidth',1.5); hold on;
 elseif ptype == 2
-    P=semilogy(X,Y,'LineWidth',2); hold on;
-    ylimits = lsylim;
+    P=semilogy(X,Y,'LineWidth',1.5); hold on;
 elseif ptype == 3
-    P=loglog(X,Y,'LineWidth',2); hold on;
-    ylimits = lsylim;
+    P=loglog(X,Y,'LineWidth',1.5); hold on;
 end
 pf=plot([freqs'; freqs'], ylimits, 'r', 'LineWidth',0.1); hold off;
+xlim([X(1) X(end)]); ylim(ylimits); uistack(P,'top');
 
-title(sprintf('N=%.f',ipks)); ylabel({'Spectral Density', '(Energy/Hz)'});
-axis tight; uistack(P,'top');
+% Create period axis on the top x-axis
+ax1 = gca;
+xt = get(ax1,'XTick');
+xp = 1./(xt*0.001); % Conversion between millihertz to seconds
+ax2=axes('Position', get(ax1, 'Position'), 'XAxisLocation','top','YTickLabel',[],'XLim',...
+         get(ax1, 'XLim'), 'XTickLabel', str2num(sprintf('%.f ', xp)),'Color','none');
+set(ax2,'xscale',xscale);set(ax1,'xticklabel',[]);
+xlabel(ax2,'Period (s)','FontSize',10);
+
+% Additional plot information
+title(sprintf('N=%.f',ipks)); ylabel(ax1,{'Spectral Density', '(Energy/Hz)'});
 ax1.XGrid = 'off'; ax1.YGrid = 'on'; 
-set(ax1,'xticklabel',[]);
 
+% Add legend if modes exist
 if exist('p1')
     legend({'Widmer & Masters'});
 end
 
+%------------------------------------------------------------------------------------------------------
 % Plot of identified peaks
+% Set up subplot
 hx2 = subplot(212);
-set(hx2,'Position',[0.15, .22, .8, .32]);
+set(hx2,'Position',[0.15, .2, .8, .32]);
 
+% Plot the data and peak identification
 if ptype == 0
-    P=plot(X,Y,'LineWidth',2); hold on;
-    ylimits = nlylim;
+    P=plot(X,Y,'LineWidth',1.5); hold on;
 elseif ptype == 1
-    P=semilogx(X,Y,'LineWidth',2); hold on;
-    ylimits = nlylim;
+    P=semilogx(X,Y,'LineWidth',1.5); hold on;
 elseif ptype == 2
-    P=semilogy(X,Y,'LineWidth',2); hold on;
-    ylimits = lsylim;
+    P=semilogy(X,Y,'LineWidth',1.5); hold on;
 elseif ptype == 3
-    P=loglog(X,Y,'LineWidth',2); hold on;
-    ylimits = lsylim;
+    P=loglog(X,Y,'LineWidth',1.5); hold on;
 end
 pl=plot([locs'; locs'], ylimits, 'k', 'LineWidth', 0.1);
 
-axis tight; uistack(P,'top');
+% Additional plot information
+xlim([X(1) X(end)]); ylim(ylimits); uistack(P,'top');
 hx2.XGrid = 'off'; hx2.YGrid = 'on';
 xlabel('Frequency (mhz)'); ylabel({'Spectral Density', '(Energy/Hz)'})
 
+% Add legend if peak frequencies exist
 if exist('p2')
     legend({'Estimated'});
 end
-
-%-------------------------------------------------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------
 % Optional output
 varns={P,pf,pl};
 varargout=varns(1:nargout);
