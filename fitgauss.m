5febe6aeabecd109f65534fa72994a77f15c5926
Page Dabney <pdabney@cressida.princeton.edu>
2021-09-28 13:29:48 -0400

    Added bounds output option

--- fitgauss.m
+++ fitgauss.m
@@ -1,5 +1,5 @@
 function varargout=fitgauss(y,x,freq,wlen,thresh)
-% [X,Y,cfit,e,res]=fitgauss(y,x,freq,wlen,thresh)
+% [X,Y,bnds,cfit,e,res]=fitgauss(y,x,freq,wlen,thresh)
 %
 % Fit gaussian distribution curve to single peak of interest.
 % Note: Only works if there is a single peak, need to look into a 
@@ -17,6 +17,7 @@ function varargout=fitgauss(y,x,freq,wlen,thresh)
 %
 % Y              Data in specifed frequency range of interest (1-D)             
 % X              Corresponding x-axis data (1-D)
+% bnds           Vector containing the upper and lower bound indexes for range of interest
 % cfit           Gaussian curve fit result, use for plotting
 % e              Root mean squared error
 % res            Residuals, as vector
@@ -26,10 +27,10 @@ function varargout=fitgauss(y,x,freq,wlen,thresh)
 % Take half the window 
 halfwin = wlen/2;
 % Obtain a frequency range of interest
-frange = [freq-halfwin freq+halfwin];
+xrange = [freq-halfwin freq+halfwin];
 
 % Obtains data arrays only within range of interest
-[X,Y] = inrange(x,y,xrange);
+[X,Y,bnds] = inrange(x,y,xrange);
 
 % Need to obtain the full width at half maximum 
 [~,~,wdt,~]=findpeaks(Y,X,'MinPeakHeight',thresh,'WidthReference','halfheight');
@@ -50,7 +51,7 @@ res = output.residuals;
 e = gof.rmse;
 
 % Optional Output
-vars={X,Y,cfit,e,res};
+vars={X,Y,bnds,cfit,e,res};
 varargout=vars(1:nargout);
 
 end
