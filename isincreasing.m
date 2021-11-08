45b1617dc7ba7ebfb567b62d375fc88a15da6379
Page Dabney <pdabney@cressida.princeton.edu>
2021-11-08 15:22:18 -0500

    Returns the upper bound if the whole array is increasing by 1

--- isincreasing.m
+++ isincreasing.m
@@ -13,7 +13,7 @@ function varargout=isincreasing(x)
 % C              Cell array of integer sequences which are increasing by 1
 % K              Upper bound indexes of the sections
 %
-% Last modified by pdabney@princeton.edu, 9/9/21
+% Last modified by pdabney@princeton.edu, 11/08/21
 
 K=[];
 % Create vector of lower bound indexes
@@ -26,22 +26,27 @@ for i = 1:length(x)-1
     end
 end
 
-% Divide segments into cells
-% Deal with the first 
-C = cell(1,length(K)+1);
-C{1} = x(1:K(1));
+if isempty(K) == 1
+    C = NaN;
+    K = x(end);
+else
+    % Divide segments into cells
+    % Deal with the first 
+    C = cell(1,length(K)+1);
+    C{1} = x(1:K(1));
+    
+    % Deal with the middle segments
+    % --- should look into removing the for loops in the future ---
+    for j = 1:length(K)-1
+        C{j+1} = x(K(j)+1:K(j+1));
+    end
+    % Deal with last segment
+    C{length(K)+1} = x(K(end)+1:end);
 
-% Deal with the middle segments
-% --- should look into removing the for loops in the future ---
-for j = 1:length(K)-1
-    C{j+1} = x(K(j)+1:K(j+1));
+    % Add index of the last element in the vector 
+    % to the upper bound index for output
+    K = [K; length(x)];
 end
-% Deal with last segment
-C{length(K)+1} = x(K(end)+1:end);
-
-% Add index of the last element in the vector 
-% to the upper bound index for output
-K = [K; length(x)];
 
 % Optional Output
 vars = {C,K};
