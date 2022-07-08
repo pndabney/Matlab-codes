2b1228544235f6d2cfb034dfdeace544b177f17a
Page Dabney <pdabney@cressida.princeton.edu>
2022-07-08 08:42:04 -0400

    Generates synthetic seismograms

--- smodel.m
+++ smodel.m
@@ -1,13 +1,13 @@
-function varargout=smodel(N,num,frange,df,len,dt,sigma1,sigma2,plotornot)
-% [S,tim,mfreqs]=smodel(N,num,frange,df,len,dt,sigma1,sigma2,plotornot)
+function varargout=smodel(N,num,frange,df,len,dt,sigma1,sigma2,plotornot,numtest)
+% [S,tim,mfreqs,a,b]=smodel(N,num,frange,df,len,dt,sigma1,sigma2,plotornot,numtest)
 %
 % Generates multiple synthetic seismograms (M x N) by specifying the 
 % coefficients. 
 %
 % Input:
 %
-% N                 Number of seismograms to create
-% num               Number of frequencies to alter power intensity
+% N                 Number of seismograms to create (default: 3)
+% num               Number of frequencies to alter power intensity 
 % frange            Vector containing 2 frequencies in order of increasing 
 %                   frequency. These specify the range of interest
 % df                Frequency step (Hz)
@@ -16,11 +16,13 @@ function varargout=smodel(N,num,frange,df,len,dt,sigma1,sigma2,plotornot)
 % sigma1, sigma2    Standard deviations for normal distributions to pick random
 %                   frequencies for generating the coefficents for the signal.
 % plotornot         1 - to plot
-%                   0 - to not plot
+%                   0 - to not plot (default)
+% numtest           Number of synthetics tests to generate (default: 31)
 %
 % Output:
 %
-% S                 Synthetic time series (M x N)
+% S                 Synthetic time series (M x N). If numtest is greater than 1, then the
+%                   output is in a cell array. If numtest is equal to 1, the output is a matrix
 % tim               Time vector (1 x M)
 % mfreqs            Frequencies with increase power intensity
 %
@@ -28,8 +30,9 @@ function varargout=smodel(N,num,frange,df,len,dt,sigma1,sigma2,plotornot)
 % 
 % Requires repository slepian_alpha. See defval.
 %
-% Last modified by pdabney@princeton.edu, 02/25/22
-defval('N',100)
+% Last modified by pdabney@princeton.edu, 04/04/22
+
+defval('N',3)
 defval('num',5)
 defval('frange',[0.001 0.05])
 defval('df',0.0001);
@@ -37,7 +40,8 @@ defval('len', 24*3600)
 defval('dt',10)
 defval('sigma1', 1)
 defval('sigma2',5)
-defval('plotornot',1)
+defval('plotornot',0)
+defval('numtest',31) 
 
 % Time vector
 tim = 0:dt:len;
@@ -50,27 +54,48 @@ M = length(freq);
 % Specific frequency indexes
 sdxab = randi(M,num,1);
 [ndxab,~]=skip(1:M,sdxab);
+while length(ndxab) ~= (M-num)
+    sdxab = randi(M,num,1);
+    [ndxab,~]=skip(1:M,sdxab);
+end
 mfreqs = freq(sdxab);
+if numtest > 1
+    S = cell(numtest,1);
+    A = cell(numtest,1);
+    B = cell(numtest,1);
+end
 
-% Pick data from a normal distribution
-fpa = randn(M-num,N)*sigma1;
-fpb = randn(M-num,N)*sigma1;
-spa = randn(num,N)*sigma2;
-spb = randn(num,N)*sigma2;
+for i = 1:numtest
+    % Pick data from a normal distribution
+    fpa = randn(M-num,N)*sigma1;
+    fpb = randn(M-num,N)*sigma1;
+    spa = randn(num,N)*sigma2;
+    spb = randn(num,N)*sigma2;
 
-% Fill the coefficients
-a = nan(M,N); b = nan(M,N);
-a(ndxab,:)=fpa; b(ndxab,:)=fpb;
-a(sdxab,:)=spa; b(sdxab,:)=spb;
+    % Fill the coefficients
+    a = nan(M,N); b = nan(M,N);
+    a(ndxab,:)=fpa; b(ndxab,:)=fpb;
+    a(sdxab,:)=spa; b(sdxab,:)=spb;
 
-% Compute signal
-S = cos(2*pi*tim(:)'.*freq(:))'*a + sin(2*pi*tim(:)'.*freq(:))'*b;
+    % Compute signal
+    ts = cos(2*pi*tim(:)'.*freq(:))'*a + sin(2*pi*tim(:)'.*freq(:))'*b;
+    
+    if numtest == 1
+        S = ts;
+        A = a;
+        B = b;
+    else
+        S{i} = ts;
+        A{i} = a;
+        B{i} = b;
+    end
+end
 
 % Optional plot
-if plotornot ==1 
+if plotornot ==1
     subplot(211)
     k = randi(N);
-    plot(tim,S(:,k))
+    plot(tim,ts(:,k))
     xlim([tim(1) tim(end)])
     xlabel('Time (s)')
     ylabel('Amplitude')
@@ -82,9 +107,10 @@ if plotornot ==1
     ylabel('Intensity')
     xlabel('Frequency (Hz)')
 end
+        
 
 % Optional Outputs
-vars={S,tim,mfreqs};
+vars={S,tim,mfreqs,A,B};
 varargout=vars(1:nargout);
 
 end
