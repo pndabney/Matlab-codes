71b3a42d24c7fadbfaf86310b2ea401cbb4dcc74
Page Dabney <pdabney@cressida.princeton.edu>
2022-07-12 01:43:58 -0400

    Provides three mthods to whiten time series

--- specwhiten.m
+++ specwhiten.m
@@ -1,21 +1,25 @@
-function varargout=specwhiten(ts,mtype, window)
-% [wts]=SPECWHITEN(ts,mtype,window)
+function varargout=specwhiten(ts,method, window)
+% [wts]=SPECWHITEN(ts,method,window)
 %
-% Whitens a time series by normalizing the spectrum by the magnitude of the amplitude
-% spectrum. Options include a 'smooth' or 'unsmooth' magnitude.
+% Whitens a time series. There are two methods: one is by normalizing the 
+% spectrum by its magnitude, or to set the spectrum values to 1 . Options for 
+% normalizing include a 'smooth' or 'unsmooth' magnitude.
 % 
 %
 % Input:
 %
 % ts            Time series data (1-D array)
-% mtype         Magnitude type: 0 unsmoothed, 1 smoothed
+% method        Whitening method: 
+%                  0 unsmoothed magnitude
+%                  1 smoothed magnitude
+%                  2 set to 1
 % window        Window length for moving average (required if mtype = 1)
 %
 % Output:
 %
 % wts           Whitened time series
 %
-% Last modified by pdabney@princeton.edu, 01/25/22
+% Last modified by pdabney@princeton.edu, 02/10/22
 
 
 % Default value
@@ -29,7 +33,11 @@ switch nargin
   case 3
     mag = movmean(abs(tsfft),window);
   case 2
-    mag = abs(tsfft);
+    if method == 0
+        mag = abs(tsfft);
+    elseif method == 2
+        mag = tsfft;
+    end
 end
 
 % Divide by amplitude spectrum and return to time domain
